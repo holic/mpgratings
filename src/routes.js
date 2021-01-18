52e3b6a6397d6a98ca3d576e0a7b1970f29468aa
Kevin Ingersoll <kingersoll@gmail.com>
2021-01-17 22:02:19 -0800

    show 10 most common makes in a list group

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const sortBy = require("lodash/sortBy");
 
 const vehicles = require("../data/vehicles.json");
+const makes = {};
+vehicles.forEach((vehicle) => {
+  makes[vehicle.make] = makes[vehicle.make] || [];
+  makes[vehicle.make].push(vehicle);
+});
+const sortedMakes = sortBy(Object.keys(makes), (make) => -makes[make].length);
 
 router.get("/", (req, res) => {
-  const makes = {};
-  vehicles.forEach((vehicle) => {
-    makes[vehicle.make] = makes[vehicle.make] || [];
-    makes[vehicle.make].push(vehicle);
-  });
   res.render("index", {
     count: vehicles.length,
-    makes: Object.keys(makes),
+    makes: sortedMakes.map((make) => ({
+      make,
+      count: makes[make].length,
+    })),
   });
 });
 
