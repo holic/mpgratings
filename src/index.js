95ac3f1fdd9f60232fc36e2492590346c80f5ce3
Kevin Ingersoll <kingersoll@gmail.com>
2021-01-17 20:35:08 -0800

    add nunjucks for view layer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.set("view engine", "html");
+nunjucks.configure(`${__dirname}/views`, {
+  autoescape: true,
+  express: app,
+});
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.render("index");
 });
 
 app.listen(port, () => {
