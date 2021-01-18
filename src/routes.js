38b90cddb35d3f7d1042c3b5b51964c1c74e7f24
Kevin Ingersoll <kingersoll@gmail.com>
2021-01-17 23:15:45 -0800

    munge data set to include slugs and links

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,54 @@
 const express = require("express");
 const router = express.Router();
-const sortBy = require("lodash/sortBy");
+const orderBy = require("lodash/sortBy");
+const kebabCase = require("lodash/kebabCase");
+const groupBy = require("lodash/groupBy");
 
-const vehicles = require("../data/vehicles.json");
-const makes = {};
-vehicles.forEach((vehicle) => {
-  makes[vehicle.make] = makes[vehicle.make] || [];
-  makes[vehicle.make].push(vehicle);
+const cars = require("../data/vehicles.json").map((car) => {
+  const slug = kebabCase(`${car.year} ${car.make} ${car.model}`);
+  const makeSlug = kebabCase(car.make);
+  const makeModel = `${car.make} ${car.model}`;
+  const makeModelSlug = kebabCase(`${car.make} ${car.model}`);
+  const yearSlug = kebabCase(car.year);
+
+  return {
+    ...car,
+    slug,
+    link: `/${slug}`,
+    makeSlug,
+    makeLink: `/makes/${makeSlug}`,
+    makeModel,
+    makeModelSlug,
+    makeModelLink: `/models/${makeModelSlug}`,
+    yearSlug,
+    yearLink: `/years/${yearSlug}`,
+  };
 });
-const sortedMakes = sortBy(Object.keys(makes), (make) => -makes[make].length);
+
+const byMake = groupBy(cars, "makeSlug");
+const byMakeModel = groupBy(cars, "makeModelSlug");
+const byYear = groupBy(cars, "yearSlug");
+
+const sortedMakes = orderBy(
+  Object.keys(byMake),
+  (make) => -byMake[make].length,
+  "desc"
+);
+const makes = sortedMakes.map((make) => {
+  const makeCars = byMake[make];
+  const car = makeCars[0];
+  return {
+    slug: make,
+    name: car.make,
+    link: car.makeLink,
+    count: makeCars.length,
+  };
+});
 
 router.get("/", (req, res) => {
   res.render("index", {
-    count: vehicles.length,
-    makes: sortedMakes.map((make) => ({
-      make,
-      count: makes[make].length,
-    })),
+    count: cars.length,
+    makes,
   });
 });
 
